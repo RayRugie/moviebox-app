a5e4767728e378afc4d36376da7d356038614a96
Omorugie Oyibotha <66238145+RayRugie@users.noreply.github.com>
2023-09-17 16:30:58 +0100

    fully responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,14 @@ import Youtube from "../assets/youtube.svg";
 
 const Footer = () => {
   return (
-    <div className="footer flex flex-col text-black space-y-6 mt-12 items-center md:items-center">
+    <div className="footer flex flex-col text-black space-y-6 mt-12 items-center md:items-center md:justify-center ">
       <div className="socials flex flex-row space-x-8 h-7">
         <a href="/"><img src={Facebook} alt="facebook" /></a>
         <a href="/"><img src={Instagram} alt="instagram" /></a>
         <a href=""><img src={Twitter} alt="twitter" /></a>
         <a href=""><img src={Youtube} alt="youtube" /></a>
       </div>
-      <div className=" text-black flex flex-col space-y-2">
+      <div className=" text-black flex flex-col space-y-2 text-center">
         <p>Conditions of Use</p>
         <p>Privacy & Policy</p>
         <p>Press Room</p>
--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,4 +1,3 @@
-// MovieDetails.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
@@ -7,13 +6,11 @@ import BounceLoader from "react-spinners/BounceLoader";
 import './MovieDetails.css'
 import Footer from './Footer';
 
-
 const MovieDetails = ({ match }) => {
   const [movie, setMovie] = useState(null);
   const [loading, setLoading] = useState(false);
- 
-let { id } = useParams();
-console.log(id)
+
+  let { id } = useParams();
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -30,7 +27,7 @@ console.log(id)
     setLoading(true);
     setTimeout(() =>{
       setLoading(false);
-    }, 10000)
+    }, 10000);
 
     fetchMovieDetails();
   }, [id]);
@@ -40,77 +37,66 @@ console.log(id)
     return <MovieGenre genre={genre.name} />;
   });
 
-  if (!movie) return <div> 
-     <BounceLoader
-  color={"#BE123C"}
-  loading={loading}
- 
-  size={150}
-  aria-label="Loading Spinner"
-  data-testid="loader"
-/></div>;
+  if (!movie) return <div className="loader">
+    <BounceLoader
+      color={"#BE123C"}
+      loading={loading}
+      size={150}
+      aria-label="Loading Spinner"
+      data-testid="loader"
+    />
+  </div>;
 
   return (
-    <>
-    {movie === null ? (
-      <div className="loading mt-52 mx-auto"></div>
-    ) : (
-      <div className="mt-3 ml-2">
-        <Link to={"/"}>
-          <p className="text-red p-2 border-2 w-fit border-red-400 rounded-xl hover:bg-red-300 duration-300">
-            Home
-          </p>
-        </Link>
-        <div>
-          <img
-            className="poster "
-            src={`https://image.tmdb.org/t/p/w1280/${movie?.backdrop_path}`}
-          alt='/'
-          />
-        </div>
+    <div className="mt-3 ml-2">
+      <Link to={"/"}>
+        <p className="back-link">
+          Home
+        </p>
+      </Link>
+      <div>
+        <img
+          className="poster"
+          src={`https://image.tmdb.org/t/p/w1280/${movie?.backdrop_path}`}
+          alt=""
+        />
+      </div>
 
-        <div className="main">
-          <div className=" flex  items-center space-x-96">
-            <div className="title flex ">
-              <div className="font-roboto flex justify-around w-3/4 md:text-sm text-2xl">
-                <p>
-                  <span data-testid="movie-title" className='s text-xl font-bold'>{movie?.title}</span> ·{" "}
-                  <span data-testid="movie-release_date">
-                 {movie ? new Date(movie.release_date).toUTCString() : ""}
- 
-                  </span>{" "}
-                  · <span data-testid="movie-runtime">{movie?.runtime}</span>m
-                </p>
-              </div>
-
-              {genres}
+      <div className="main">
+        <div className="title-section">
+          <div className="title">
+            <div className="font-roboto">
+              <p>
+                <span className="movie-title">{movie?.title}</span> ·{" "}
+                <span className="movie-release_date">
+                  {movie ? new Date(movie.release_date).toUTCString() : ""}
+                </span>{" "}
+                · <span className="movie-runtime">{movie?.runtime}</span>m
+              </p>
             </div>
-            <p className="text-sm font-cursive items-end justify-end">
+            <p className="rating">
               {movie.vote_average}/10 | {movie.popularity}
             </p>
           </div>
-          <div className="flex sm:text-sm sm:flex-col justify-between">
-            <div className="desc mt-4 ">
-              <p data-testid="movie-overview" className="mb-8">
-                {movie?.overview}
-              </p>
-            </div>
-            <div className="side space-x-7">
-              <button className="w-52 p-2 rounded-md  mb-2 border-none bg-pink-800 border-2">
-                See Showtimes
-              </button>
-              <button className="w-52  rounded-md border-solid border-pink-800 p-2 bg-pink-300 bg-opacity-50 border-2">
-                More watch options
-              </button>
-            </div>
-          </div>
+          {genres}
         </div>
-        <Footer />
+        <div className="desc mt-4">
+          <p className="movie-overview">
+            {movie?.overview}
+          </p>
+        </div>
+        <div className="buttons mt-4">
+          <button className="showtimes-btn">
+            See Showtimes
+          </button>
+          <button className="watch-options-btn">
+            More watch options
+          </button>
+        </div>
       </div>
-    )}
-  </>
-);
+      <Footer />
+    </div>
+  );
 };
-
 
 export default MovieDetails;
